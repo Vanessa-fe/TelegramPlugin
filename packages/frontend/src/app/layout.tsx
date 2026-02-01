fe8590da2309f62fb3d78c3f3e59bceb2287b1c2
vferrand-smid <vferrand@smartphone-id.com>
2026-02-01 12:25:00 +0100

    retrait notif toaster

--- packages/frontend/src/app/layout.tsx
+++ packages/frontend/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { AuthProvider } from '@/contexts/auth-context';
-import { Toaster } from '@/components/ui/sonner';
 import { NextIntlClientProvider } from 'next-intl';
 import { getLocale, getMessages } from 'next-intl/server';
 
@@ -37,7 +36,6 @@ export default async function RootLayout({
         <NextIntlClientProvider messages={messages}>
           <AuthProvider>
             {children}
-            <Toaster />
           </AuthProvider>
         </NextIntlClientProvider>
       </body>
