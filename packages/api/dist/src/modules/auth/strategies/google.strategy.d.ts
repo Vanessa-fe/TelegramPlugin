94b812c5d8b2517dddb995fcbd63fe74c9601fff
vferrand-smid <vferrand@smartphone-id.com>
2026-02-08 15:58:49 +0100

    Google Auth ok

--- packages/api/dist/src/modules/auth/strategies/google.strategy.d.ts
+++ packages/api/dist/src/modules/auth/strategies/google.strategy.d.ts
@@ -7,7 +7,7 @@ export interface GoogleProfile {
     firstName?: string;
     lastName?: string;
 }
-declare const GoogleStrategy_base: new (...args: [options: import("passport-google-oauth20").StrategyOptionsWithRequest] | [options: import("passport-google-oauth20").StrategyOptions] | [options: import("passport-google-oauth20").StrategyOptions] | [options: import("passport-google-oauth20").StrategyOptionsWithRequest]) => Strategy & {
+declare const GoogleStrategy_base: new (...args: [options: import("passport-google-oauth20").StrategyOptions] | [options: import("passport-google-oauth20").StrategyOptions] | [options: import("passport-google-oauth20").StrategyOptionsWithRequest] | [options: import("passport-google-oauth20").StrategyOptionsWithRequest]) => Strategy & {
     validate(...args: any[]): unknown;
 };
 export declare class GoogleStrategy extends GoogleStrategy_base {
